aad2a154393918d5bcd02a7890b38d265157f7c5
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2021-01-30 12:27:01 -0300

    SSG Static Site Generation

--- src/pages/users.js
+++ src/pages/users.js
@@ -1,30 +1,29 @@
-import {useEffect, useState} from 'react';
-
 import axios from 'axios';
+import Link from 'next/link';
+function Users({users}) {
 
-function Users({ users }) {
-  const [users, setUsers] = useState([])
+  return (
+    <div>
+     {
+      users.map( user => (
+        <div>
+          <Link href="/profile/[id]" as={`/profile/${user.id }`}>
+            <a>{user.name}</a>
+          </Link> 
+        </div>
+      ))}
+  </div>);
 
-  const fetchUsers = async () => {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-    const data = await response.data;
-    
-    setUsers(data);
-  };
-
-  console.log(users)
-
-  useEffect(()=>{
-    fetchUsers()
-  }, [])
-
-  return (<div>
-   {
-     users.map( user => (
-       <div>{user.name}</div>
-     ))
-   }
-  </div>)
 }
 
-export default Users;
+export async function getStaticProps(context) {
+  const response = await axios.get(
+    `https://jsonplaceholder.typicode.com/users`
+  );
+  const data = await response.data;
+  return {
+    props: { users: data}, // will be passed to the page component as props
+  }
+}
+
+export default Users
