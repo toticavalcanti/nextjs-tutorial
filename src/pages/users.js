25d453870deaadf778fd9c394c152a8eab6f649d
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2021-01-30 09:19:03 -0300

    get data SSR

--- src/pages/users.js
+++ src/pages/users.js
@@ -3,22 +3,7 @@ import {useEffect, useState} from 'react';
 import axios from 'axios';
 
 function Users({users}) {
-  // const [users, setUsers] = useState([])
-
-  // const fetchUsers = async () => {
-  //   const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-  //   const data = await response.data;
-    
-  //   setUsers(data);
-  // };
-
-  // console.log(users)
-
-  // useEffect(()=>{
-  //   fetchUsers()
-  // }, [])
-
-  return (
+   return (
     <div>
      {
       users.map( user => (
